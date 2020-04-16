a71da7c749557ae67776d3a10a31bbf1c6137dc8
root <root@localhost.localdomain>
2020-04-16 22:16:20 +0000

    ADDED: Add Part Time Employee & Wage

--- employeeWage.java
+++ employeeWage.java
@@ -2,21 +2,20 @@ public class employeeWage {
 	public static void main(String args[]){
 		//constant
 		final int fullTime = 1;
+		final int partTime = 2;
 		final int wagePerHour = 20;
 		final int fullDayHour = 8;
 		//variable
 		int dailyWage;
 		int dailyHours;
 		//computation
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if ( empCheck == fullTime ){
-			System.out.println("Employee is Present");
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if ( empCheck == fullTime )
 			dailyHours = fullDayHour;
-		}
-		else {
-			System.out.println("Employee is Absent");
+		else if ( empCheck == partTime )
 			dailyHours = 0;
-		}
+		else
+			dailyHours = fullDayHour / 2;
 		System.out.println("Employee daily wage is "+(wagePerHour * dailyHours));
 	}
 } 

1f73cbdc995229bc40dbce2c79983fce9c67ea0c
root <root@localhost.localdomain>
2020-04-16 22:36:58 +0000

    ADDED: Calculating Wages For Month

--- employeeWage.java
+++ employeeWage.java
@@ -5,22 +5,29 @@ public class employeeWage {
 		final int partTime = 2;
 		final int wagePerHour = 20;
 		final int fullDayHour = 8;
+		final int dayPerMonth = 20;
 		//variable
 		int dailyWage;
 		int dailyHours;
+		int monthlyWage = 0;
 		//computation
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-			case fullTime:
-				dailyHours = fullDayHour;
-				break;
-			case partTime:
-				dailyHours = fullDayHour / 2;
-				break;
-			default:
-				dailyHours = 0;
-				break;
+		for ( int day=0 ; day<=dayPerMonth ; day++ )
+		{
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+				case fullTime:
+					dailyHours = fullDayHour;
+					break;
+				case partTime:
+					dailyHours = fullDayHour / 2;
+					break;
+				default:
+					dailyHours = 0;
+					break;
+			}
+			dailyWage = wagePerHour * dailyHours;
+			monthlyWage += dailyWage;
 		}
-		System.out.println("Employee daily wage is "+(wagePerHour * dailyHours));
+		System.out.println("Employee monthly wage is "+monthlyWage);
 	}
 } 

6f54c931421b97b2b755a144b28428238a598dc2
root <root@localhost.localdomain>
2020-04-16 22:08:15 +0000

    ADDED: Calculate Daily Employee Wage

--- employeeWage.java
+++ employeeWage.java
@@ -1,13 +1,22 @@
 public class employeeWage {
 	public static void main(String args[]){
-
 		//constant
-		final int fullTime=1;
+		final int fullTime = 1;
+		final int wagePerHour = 20;
+		final int fullDayHour = 8;
+		//variable
+		int dailyWage;
+		int dailyHours;
 		//computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if ( empCheck == fullTime )
+		if ( empCheck == fullTime ){
 			System.out.println("Employee is Present");
-		else
+			dailyHours = fullDayHour;
+		}
+		else {
 			System.out.println("Employee is Absent");
+			dailyHours = 0;
+		}
+		System.out.println("Employee daily wage is "+(wagePerHour * dailyHours));
 	}
 } 

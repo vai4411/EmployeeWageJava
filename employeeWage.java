9efe4cc2922c64d7e07e64be442a62e1d9c61627
root <root@localhost.localdomain>
2020-04-16 21:51:06 +0000

    Initial Commit

---
+++ employeeWage.java
@@ -0,0 +1,12 @@
+public class employeeWage {
+	public static void main(String args[]){
+		//constant
+		final int fullTime=1;
+		//computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if ( empCheck == fullTime )
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+	}
+} 
